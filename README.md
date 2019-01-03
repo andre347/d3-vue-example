d76b84f74cd2655889d437b4a3ff2110b159723b
andre347 <aedevries19@gmail.com>
2019-01-03 15:53:26 +0000

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # d3-vue-example
 
-Repository supporting the 'D3.js and Vue.js' blog post on Medium. Link [here]()
+Repository supporting the 'D3.js and Vue.js' blog post on Medium. Link [here](https://medium.com/@andre.devries/d3-js-and-vue-js-7a6a721eb79f)
 
 ## Project setup
 
--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:andre347/d3-vue-example.git master:gh-pages
+git push -f https://github.com/andre347/d3-vue-example.git master:gh-pages
 
 cd -
